8cebb2af9371b601f29d1fddbd96820e360922b5
Colin Kearns <ckearns1210@gmail.com>
2015-11-08 17:19:34 -0500

    Before styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.destroy_all
+Party.destroy_all
+Item.destroy_all
+
+User.create([{name: "Colin Kearns", email: "ckearns1210@gmail.com", username: "Colin", password: "Colin", password_confirmation: "Colin"}])
+User.create([{name: "Cyrus", email: "cyrus.@gmail.com", username: "Cyrus", password: "Cyrus", password_confirmation: "Cyrus"}])
+User.create([{name: "Guy", email: "guy@gmail.com", username: "Guy", password: "Guy", password_confirmation: "Guy"}])
+User.create([{name: "Zoe", email: "zoe@gmail.com", username: "Zoe", password: "Zoe", password_confirmation: "Zoe"}])
+User.create([{name: "Kate", email: "kate@gmail.com", username: "Kate", password: "Kate", password_confirmation: "Kate"}])
